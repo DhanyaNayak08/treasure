5e8f8507924a1e4cd539909307463eef71652e6e
DhanyaNayak08 <87807197+DhanyaNayak08@users.noreply.github.com>
2021-11-17 21:23:10 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,14 @@
+class System{
+
+    constructor(){}
+
+    // Add code to authenticate the given code and the access codes.
+     authenticte(actualCoode,enterCode){
+         if(actualCode === enterCode)
+            return true
+    else
+            return false
+     }
+    
+
+}
